75e575dd9514f14e18d1f080c24212a5408fe4b8
L-Evason <evasonlewis@gmail.com>
2025-03-27 14:43:03 +0000

    Added functionality to update and delete data

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from models import *
 
 main = Blueprint('main', __name__)
@@ -14,9 +14,19 @@ def index():
 def create_routine():
     # If createing a routine get 'name'. Upload to .db file
     if request.method == 'POST':
+        # Check if routine_id exists
+        routine_id = request.form.get('routine_id') 
         routine_name = request.form['name']
-        new_routine = Routine(name=routine_name)
-        db.session.add(new_routine)
+        # check if routine exists
+        if routine_id:
+            routine = Routine.query.get_or_404(routine_id)
+            # update routine name
+            routine.name = routine_name
+        
+        else:
+
+            new_routine = Routine(name=routine_name)
+            db.session.add(new_routine)
         db.session.commit()
     
     routines = Routine.query.all()
@@ -45,6 +55,13 @@ def view_routine(routine_id):
         
     return render_template('routine.html', routine=routine,
         exercises= exercises, routine_exercises=routine_exercises)
+
+@main.route('/routine_delete/<int:routine_id>', methods=['POST'])
+def delete_routine(routine_id):
+    routine = Routine.query.get_or_404(routine_id)
+    db.session.delete(routine)
+    db.session.commit()
+    return redirect(url_for('main.create_routine'))
 
 @main.route('/routine_log/<int:routine_id>', methods=['GET', 'POST'])
 def log_routine(routine_id):
@@ -97,28 +114,47 @@ def muscle_groups():
         db.session.add(new_group)
         db.session.commit()
 
-        return redirect(url_for('main.add_muscle_to_group',
+        return redirect(url_for('main.update_or_add_muscle_to_group',
             muscle_group_id=new_group.id))
 
     muscle_groups = MuscleGroup.query.all()
     return render_template('muscle_groups.html',
         groups=muscle_groups)
 
+@main.route('/delete_muscle_group/<int:muscle_group_id>',
+            methods=['POST'])
+def delete_muscle_group(muscle_group_id):
+    selected_group = MuscleGroup.query.get_or_404(muscle_group_id)
+    muscles = Muscle.query.filter_by(
+        muscle_group_id=muscle_group_id).all()
+    for muscle in muscles:
+        db.session.delete(muscle)
+    db.session.delete(selected_group)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
 @main.route('/muscle_group/<int:muscle_group_id>',
     methods=['GET', 'POST'])
-def add_muscle_to_group(muscle_group_id):
+def update_or_add_muscle_to_group(muscle_group_id):
     muscle_group = MuscleGroup.query.get_or_404(muscle_group_id)
     if request.method == 'POST':
+        # Check if muscle_id exists
+        muscle_id = request.form.get('muscle_id')  
         muscle_name = request.form['name']
+
+        if muscle_id:
+            muscle = Muscle.query.get_or_404(muscle_id)
+            muscle.name = muscle_name
+        else:
+            new_muscle = Muscle(name=muscle_name,
+                muscle_group_id=muscle_group.id)
+            db.session.add(new_muscle)
         
-        new_muscle = Muscle(name=muscle_name,
-            muscle_group_id=muscle_group.id)
         
-        db.session.add(new_muscle)
         db.session.commit()
         return redirect(
             url_for(
-                'main.add_muscle_to_group',
+                'main.update_or_add_muscle_to_group',
                 muscle_group_id=muscle_group.id))
     
     # Display the current muscles in the group
@@ -128,19 +164,42 @@ def add_muscle_to_group(muscle_group_id):
     return render_template('muscle_group.html',
         muscle_group=muscle_group, muscles=muscles_in_group)
 
+@main.route('/delete_muscle/<int:muscle_group_id>/<int:muscle_id>',
+            methods=['POST'])
+def delete_muscle(muscle_group_id, muscle_id):
+    selected_muscle = Muscle.query.get_or_404(muscle_id)
+    db.session.delete(selected_muscle)
+    db.session.commit()
+    return redirect(url_for(
+        'main.update_or_add_muscle_to_group',
+        muscle_group_id=muscle_group_id
+    ))
+
 
 @main.route('/exercises', methods=['GET', 'POST'])
 def exercises():
     if request.method == 'POST':
+        exercise_id = request.form.get('exercise_id')
         exercise_name = request.form['name']
-        new_exercise = Exercise(name=exercise_name)
-        db.session.add(new_exercise)
+        if exercise_id:
+            exercise = Exercise.query.get_or_404(exercise_id)
+            exercise.name = exercise_name
+        else:
+            new_exercise = Exercise(name=exercise_name)
+            db.session.add(new_exercise)
         db.session.commit()
     
-    exercises = Exercise.query.all()
+    exercises = Exercise.query.options(db.joinedload(Exercise.muscles_worked)).all()
     sets = SetLog.query.all()
     return render_template('exercises.html',
         exercises=exercises, sets=sets)
+
+@main.route('/delete_exercise/<int:exercise_id>', methods=['POST'])
+def delete_exercise(exercise_id):
+    selected_exercise= Exercise.query.get_or_404(exercise_id)
+    db.session.delete(selected_exercise)
+    db.session.commit()
+    return redirect(url_for('main.exercises'))
 
 @main.route('/exercise/<int:exercise_id>', methods=['GET', 'POST'])
 def view_exercise(exercise_id):
